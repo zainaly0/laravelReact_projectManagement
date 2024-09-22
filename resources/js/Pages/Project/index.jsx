e7b54233005ce43af60ca766115981f8c1e85804
zainaly0 <zaidk4076@gmail.com>
2024-09-22 21:58:08 +0530

    created files, routes and dashboard link

---
+++ resources/js/Pages/Project/index.jsx
@@ -0,0 +1,9 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import React from "react";
+
+const index = () => {
+     return <AuthenticatedLayout>
+     </AuthenticatedLayout>;
+};
+
+export default index;
--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,21 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/dashboard',fn() => Inertia::render('Dashboard'))
+    ->name('dashboard');
+
+    Route::resource('project', ProjectController::class);
+    Route::resource('task', TaskController::class);
+    Route::resource('user', UserController::class);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
