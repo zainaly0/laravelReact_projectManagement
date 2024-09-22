482f8376daaa796b79804693fef0cfe1c8f4eb13
zainaly0 <zaidk4076@gmail.com>
2024-09-22 20:30:49 +0530

    migration file, factory and seeder are complete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Task;
+
 
 class Project extends Model
 {
     use HasFactory;
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// use Database\Seeders\StudentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        // $this->call([
+        //     StudentSeeder::class
+        // ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'zaid',
+            'email' => 'zaid@example.com',
+            'password' => bcrypt('123.321A'),
+            'email_verified_at' => time(),
+
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
     }
 }
