0a9b7f0ddf36283e8813638aa96caf5c22785f7e
zainaly0 <zaidk4076@gmail.com>
2024-09-26 23:42:48 +0530

    constaints for css and others

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,13 +1,14 @@
 import { Link } from '@inertiajs/react'
 import React from 'react'
 
-const Pagination = ({ Links }) => {
+const Pagination = ({ links }) => {
+    console.log(links)
     return (
         <div className='text-center mt-4'>
             {
-                Links.map((link) => (
+                links.map((link) => (
                     <Link
-                    preserveScroll
+                        preserveScroll
                         href={link.url || ""}
                         key={link.label}
                         className={'inline-block py-2 px-3 rounded-lg text-gray-200 text-xs ' + (link.active ? "bg-gray-950 " : " ") + (!link.url ? "!text-gray-500 cursor-not-allowed " : "hover:bg-gray-950")}
