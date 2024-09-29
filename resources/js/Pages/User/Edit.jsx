f8eff5284633a03e92b9533e7d8c0e7cfa3faf8d
zainaly0 <zaidk4076@gmail.com>
2024-09-29 18:55:56 +0530

    duplicate Project file and replace whole file keywords with ctr+shift+h project to user

---
+++ resources/js/Pages/User/Edit.jsx
@@ -0,0 +1,140 @@
+import InputError from '@/Components/InputError'
+import InputLabel from '@/Components/InputLabel'
+import SelectInput from '@/Components/SelectInput'
+import TextAreaInput from '@/Components/TextAreaInput'
+import TextInput from '@/Components/TextInput'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import { Head, Link, useForm } from '@inertiajs/react'
+import React from 'react'
+
+const Edit = ({ auth, user }) => {
+  const { data, setData, post, errors, reset } = useForm({
+    image:"" ,
+    image_path: user.image_path || "" ,
+    name: user.name || "" ,
+    status: user.status || "" ,
+    description: user.description || "" ,
+    due_date: user.due_date || "" ,
+    _method: "PUT",
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    post(route('user.update', user.id));
+  }
+
+
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <div className="flex justify-between items-center">
+          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Edit User:  "{user.name}""
+          </h2>
+
+        </div>
+      }
+    >
+      <Head title="Users" />
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <form onSubmit={onSubmit} action="" className='p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg'>
+              <pre>
+                {user.image_path && <div className='mt-4'>
+                  <img src={user.image_path} className='W-64' style={{height:'100px'}} />
+                  
+                  </div>}
+              </pre>
+
+              {/* <pre>
+                {JSON.stringify(data, undefined, 2)}
+              </pre> */}
+              <div>
+                <InputLabel htmlFor="user_image_path" value="User image" />
+                <TextInput
+                  id="user_image_path"
+                  type="file"
+                  name="image"
+                  // value={data.image}
+                  className="mt-1 block w-full"
+                  onChange={e => setData('image', e.target.files[0])}
+                />
+                <InputError message={errors.image} className='mt-2' />
+              </div>
+              <div>
+                <InputLabel htmlFor="user_name" value="User Name" />
+                <TextInput
+                  id="user_name"
+                  type="text"
+                  name="name"
+                  value={data.name}
+                  isFocused={true}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData('name', e.target.value)}
+                />
+                <InputError message={errors.name} className='mt-2' />
+              </div>
+              <div>
+                <InputLabel htmlFor="user_description" value="User Description" />
+                <TextAreaInput
+                  id="user_description"
+                  name="user_description"
+                  value={data.description}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData('description', e.target.value)}
+                />
+                <InputError message={errors.description} className='mt-2' />
+              </div>
+              <div>
+                <InputLabel htmlFor="user_due_data" value="User DeadLine" />
+                <TextInput
+                  id="user_due_data"
+                  type="date"
+                  name="due_date"
+                  value={data.due_date}
+                  isFocused={true}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData('due_date', e.target.value)}
+                />
+                <InputError message={errors.due_date} className='mt-2' />
+              </div>
+              <div>
+                <InputLabel htmlFor="user_status" value="User Status" />
+                <SelectInput
+                  name="status"
+                  id="user_status"
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData('status', e.target.value)}
+                >
+                  <option value="">Select User</option>
+                  <option value="pending">Pending</option>
+                  <option value="in_progress">In Progress</option>
+                  <option value="complete">Complete</option>
+                </SelectInput>
+                <InputError message={errors.user_status} className='mt-2' />
+              </div>
+
+              <div className='mt-4 text-right'>
+                <Link href={route('user.index')} className='bg-gray-100 py-1 px-3 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2'>
+                  Cancel
+                </Link>
+                <button className='bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600'>
+                  Submit
+                </button>
+
+              </div>
+
+
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </AuthenticatedLayout>
+  )
+}
+
+export default Edit
